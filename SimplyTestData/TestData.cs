7127d6c625d73808b3dfe941d6cbbe3aacdbe8e3
Igor Toporet <igort@iqmetrix.com>
2012-06-04 10:21:56 -0700

    rearranged class members;  added few XML comments

--- SimplyTestData/TestData.cs
+++ SimplyTestData/TestData.cs
@@ -27,6 +27,73 @@ namespace SimplyTestData
         }
 
         /// <summary>
+        /// Creates object of specified type <typeparam name="T">T</typeparam>
+        /// with all the type-applicable permanent customizations applied
+        /// as well as passed ad-hoc ones.
+        /// </summary>
+        /// <typeparam name="T">Type of object to be created.</typeparam>
+        /// <param name="customizations">Ad-hoc customizations to be applied to resulting object.</param>
+        /// <returns>
+        /// Object of type <typeparam name="T">T</typeparam>
+        /// with all type-applicable permanent customizations applied
+        /// as well as passed ad-hoc ones.
+        /// </returns>
+        public static T Create<T>(params Action<T>[] customizations) where T : class, new()
+        {
+            var obj = new T();
+
+            obj.Customize(GetApplicablePermanentCustomizations<T>());
+            obj.Customize(customizations);
+
+            return obj;
+        }
+
+        /// <summary>
+        /// Creates list of objects of specified type <typeparam name="T">T</typeparam>
+        /// with all the type-applicable permanent customizations applied
+        /// as well as passed ad-hoc ones.
+        /// </summary>
+        /// <typeparam name="T">Type of object to be created.</typeparam>
+        /// <param name="size">List capacity (i.e. number of objects in list)</param>
+        /// <returns>
+        /// List of objects of specified type <typeparam name="T">T</typeparam>
+        /// with all the type-applicable permanent customizations applied
+        /// as well as passed ad-hoc ones.
+        /// </returns>
+        public static IList<T> CreateListOf<T>(ushort size) where T : class, new()
+        {
+            var result = new List<T>(size);
+            for (int i = 0; i < size; i++)
+            {
+                result.Add(Create<T>());
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Customizes object by applying all the type-applicable permanent customizations
+        /// as well as passed ad-hoc ones.
+        /// </summary>
+        /// <typeparam name="T">Type of object beig customized.</typeparam>
+        /// <param name="obj">Object to be customized.</param>
+        /// <param name="customizations">Ad-hoc customizations to be applied to passed object</param>
+        /// <returns>
+        /// Object with all the type-applicable permanent customizations
+        /// applied as well as passed ad-hoc ones.
+        /// </returns>
+        public static T Customize<T>(this T obj, params Action<T>[] customizations)
+        {
+            var uniqueInvocations = customizations.SelectMany(c => c.GetInvocationList()).Distinct();
+
+            foreach (Action<T> action in uniqueInvocations)
+            {
+                action(obj);
+            }
+
+            return obj;
+        }
+
+        /// <summary>
         /// Stores customizations permanently and then applies them
         /// to every created object of type <typeparam name="T">T</typeparam> or of derived type.
         /// </summary>
@@ -40,36 +107,9 @@ namespace SimplyTestData
             }
         }
 
-        public static T Create<T>(params Action<T>[] customizations) where T : class
+        private static bool CanInterpretTypeAsCustomized(Type type, Type customizedType)
         {
-            var obj = (T) Activator.CreateInstance(typeof(T));
-
-            obj.Customize(GetApplicablePermanentCustomizations<T>());
-            obj.Customize(customizations);
-
-            return obj;
-        }
-
-        public static IList<T> CreateListOf<T>(ushort size) where T : class
-        {
-            var result = new List<T>(size);
-            for (int i = 0; i < size; i++)
-            {
-                result.Add(Create<T>());
-            }
-            return result;
-        }
-
-        public static T Customize<T>(this T obj, params Action<T>[] customizations)
-        {
-            var uniqueInvocations = customizations.SelectMany(c => c.GetInvocationList()).Distinct();
-
-            foreach (Action<T> action in uniqueInvocations)
-            {
-                action(obj);
-            }
-
-            return obj;
+            return customizedType.IsAssignableFrom(type) || type.GetInterfaces().Contains(customizedType);
         }
 
         private static Action<T>[] GetApplicablePermanentCustomizations<T>()
@@ -86,11 +126,6 @@ namespace SimplyTestData
             return applicableCustomizations.Cast<Action<T>>().ToArray();
         }
 
-        private static bool CanInterpretTypeAsCustomized(Type type, Type customizedType)
-        {
-            return customizedType.IsAssignableFrom(type) || type.GetInterfaces().Contains(customizedType);
-        }
-
         private static void StoreCustomizationAsPermanent<T>(Action<T> customization) where T : class
         {
             Type actualUnderlyingType = customization.GetType().GetGenericArguments()[0];
@@ -98,8 +133,10 @@ namespace SimplyTestData
             Delegate storedPermanentCustomizations;
             if (PermanentCustomizations.TryGetValue(actualUnderlyingType, out storedPermanentCustomizations))
             {
-                var unique = storedPermanentCustomizations.GetInvocationList().Union(customization.GetInvocationList());
+                var unique = storedPermanentCustomizations.GetInvocationList()
+                    .Union(customization.GetInvocationList());
                 var combined = Delegate.Combine(unique.ToArray());
+
                 PermanentCustomizations[actualUnderlyingType] = combined;
             }
             else
