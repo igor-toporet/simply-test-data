83d1c1a7e564274ebc10d45135693f494b780669
Igor Toporet <igor.toporet@gmail.com>
2012-06-04 23:31:24 -0700

    added GettingStarted project

---
+++ GettingStarted/BasicFixture.cs
@@ -0,0 +1,30 @@
+﻿// ReSharper disable InconsistentNaming
+
+using SimplyTestData;
+using Xunit;
+
+namespace GettingStarted
+{
+    public class BasicFixture
+    {
+        private class Greeting
+        {
+            public string Message { get; set; }
+        }
+
+        private void HelloWorld(Greeting greeting)
+        {
+            greeting.Message = "Hello, World!";
+        }
+
+        [Fact]
+        public void Example_01_HelloWorld()
+        {
+            var greeting = TestData.Create<Greeting>(HelloWorld);
+
+            Assert.Equal("Hello, World!", greeting.Message);
+        }
+    }
+}
+
+// ReSharper restore InconsistentNaming
